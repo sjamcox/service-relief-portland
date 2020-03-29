b2f112b78fd46769c62fc379d156870029ec7350
Sam Cox <sam@coxdesign.co>
2020-03-28 22:12:42 -0700

    Initial commit from gatsby: (https://github.com/smarterlabs/service-relief-evansville.git)

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+require(`dotenv`).config({ silent: true })
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-service-relief`,
+      options: {
+        authorName: `Smarter Labs`,
+        authorLink: `https://smarterlabs.com/`,
+      },
+    },
+    {
+		 resolve: `gatsby-plugin-google-analytics`,
+      options: {
+			trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+  ],
+}
